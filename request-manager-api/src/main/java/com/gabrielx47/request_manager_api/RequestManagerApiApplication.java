e9ec53ad714e12e9f63e08c277fca426735a0d42
Gabriel Mendonça <galeraxxx76@gmail.com>
2026-05-01 22:47:13 -0300

    chore: adicionar aplicação frontend e backend

---
+++ request-manager-api/src/main/java/com/gabrielx47/request_manager_api/RequestManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielx47.request_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestManagerApiApplication.class, args);
+	}
+
+}
---
+++ request-manager-api/src/test/java/com/gabrielx47/request_manager_api/RequestManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielx47.request_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
