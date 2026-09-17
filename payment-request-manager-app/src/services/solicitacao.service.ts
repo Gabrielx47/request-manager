76e8c3f07ff04eccaa19a10ba0845549a935af25
Gabriel Mendonça <galeraxxx76@gmail.com>
2026-09-17 00:25:05 -0300

    feat: adiciona função para atualizar o status da solicitação de pagamento

--- payment-request-manager-app/src/services/solicitacao.service.ts
+++ payment-request-manager-app/src/services/solicitacao.service.ts
@@ -21,4 +21,23 @@ async function buscarTodosOsDadosDaSolicitacao(id: number) {
   }
 }
 
-export { buscarTodosOsDadosDaSolicitacao };
+async function atualizarStatusDaSolicitacao(id: number, novoStatus: string) {
+  try {
+    const response = await axios.patch(`${baseUrl}/solicitacoes/${id}`, { status: novoStatus });
+
+    return {
+      sucesso: true,
+      mensagem: response.data
+    };
+  } catch (error: any) {
+    const mensagem = error.response?.data?.detail || 'Erro ao atualizar status';
+    console.error('Erro ao atualizar status da solicitação:', error);
+
+    return {
+      sucesso: false,
+      mensagem
+    };
+  }
+}
+
+export { buscarTodosOsDadosDaSolicitacao, atualizarStatusDaSolicitacao };
